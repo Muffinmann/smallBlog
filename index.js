e5347c4ffb7f7abdd1efcb2dcc5f1fdb4297c2fc
conner <conner.zhang.gl@gmail.com>
2022-09-21 22:02:50 +0200

    wip

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const app = require('express')();
 const { v4 } = require('uuid');
+const PORT = 3000
 
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
 app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
