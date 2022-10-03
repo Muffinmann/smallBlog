042a3e90934da424577bc6058a967b15dd366122
conner <conner.zhang.gl@gmail.com>
2022-10-03 22:03:39 +0200

    wip

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ const app = require('express')();
 const { v4 } = require('uuid');
 const fs = require('fs');
 
+const getFilePath = (name) => path.join('/', __dirname, name);
+
 app.get('/', (req, res) => {
-  fs.readFile(getFilePath('home.html'), (err, data) => {
+  fs.readFile(path.join('/', __dirname, '..', 'index.html'), (err, data) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
--- app.mjs
+++ app.mjs
@@ -9,12 +9,15 @@ const __dirname = path.dirname(__filename);
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const getFilePath = (name) => path.join('/', __dirname, 'data', name);
+const getFilePath = (name) => path.join('/', __dirname, name);
 
 const server = http.createServer((req, res) => {
   console.log('req:', req.method, ' for', req.url);
   if (req.url === '/' && req.method === 'GET') {
-    fs.readFile(getFilePath('home.html'), (err, data) => {
+    fs.readFile(getFilePath('index.html'), (err, data) => {
+      if (err) {
+        console.log(err)
+      }
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/html');
       res.end(data);
