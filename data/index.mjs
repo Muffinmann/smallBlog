107500961bf06a317c38fb7d35ad0e04b66aa578
conner <conner.zhang.gl@gmail.com>
2022-11-16 22:07:38 +0100

    wip

--- data/index.mjs
+++ data/index.mjs
@@ -94,11 +94,12 @@ const db = new sqlite3.Database('./data/smallBlog.db', sqlite3.OPEN_READWRITE, (
 //   console.log('Closed db successfully.')
 // })
 export const getBlogs = (cb) => {
+  console.log('getting blogs...');
   db.all(`SELECT blog_id, blog_title FROM blogs`, (err, rows) => {
     if (err) {
       return console.error('select error: ', err.message);
     }
-    // console.log('rows = ', rows);
+    console.log('rows = ', rows);
     cb(rows);
     // db.close()
   });
